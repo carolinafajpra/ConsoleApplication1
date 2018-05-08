28d888a005561b0a8f1af35972a99f626b8192e5
Carolina Fajardo Prado <cfajardo@mediainteractiva.com>
2018-05-08 10:25:22 +0200

    commit comentarios

--- ConsoleApplication1/Natacion.cs
+++ ConsoleApplication1/Natacion.cs
@@ -40,18 +40,24 @@ namespace ConsoleApplication1
         [Test]
         public void ThePruebaNatacionTest()
         {
+            Console.WriteLine("✔ Navegando a http://natacioncs.com/blog/");
             driver.Navigate().GoToUrl("http://natacioncs.com/blog/");
+
+            Console.WriteLine("✔ Click para navegar");
             driver.FindElement(By.XPath("//div[@id='page-content']/div[2]/header")).Click();
             driver.FindElement(By.XPath("//div[@id='page-content']/div[2]/header/p")).Click();
             driver.FindElement(By.XPath("//div[@id='page-content']/div[2]/header/p")).Click();
             try
             {
+                Console.WriteLine("✔ Comparando si el texto aparece");
                 Assert.AreEqual("Blog de natación creado por el equipo de NatacionCS y sus atletas para los apasionados a la natación.", driver.FindElement(By.XPath("//div[@id='page-content']/div[2]/header/p")).Text);
             }
             catch (AssertionException e)
             {
                 verificationErrors.Append(e.Message);
             }
+
+            Console.WriteLine("✔ Buscando art'iculo: la importancia de los bañadores");
             driver.FindElement(By.XPath("//div[@id='postlist']/article/div[2]")).Click();
             Assert.AreEqual("La importancia del tejido en los bañadores de natación", driver.FindElement(By.LinkText("La importancia del tejido en los bañadores de natación")).Text);
         }
